22855b4c9eb9cef5b83b20c9708429709326f282
Taegyun Kim <k.taegyun@gmail.com>
2021-01-29 13:46:46 -0500

    openssl-rsa sign and verify example

---
+++ examples/openssl-rsa.rs
@@ -0,0 +1,30 @@
+extern crate openssl;
+
+use openssl::hash::MessageDigest;
+use openssl::pkey::PKey;
+use openssl::rsa::Rsa;
+use openssl::sign::Signer;
+use openssl::sign::Verifier;
+
+fn main() {
+    let data0 = b"hello, world!";
+    let data1 = b"hola, mundo!";
+    println!("Data0 size in bytes: {}", std::mem::size_of_val(data0));
+    println!("Data1 size in bytes: {}", std::mem::size_of_val(data1));
+
+    // Generate a keypair
+    let keypair = Rsa::generate(1024).unwrap();
+    let keypair = PKey::from_rsa(keypair).unwrap();
+
+    // Sign the data
+    let mut signer = Signer::new(MessageDigest::sha256(), &keypair).unwrap();
+    signer.update(data0).unwrap();
+    // signer.update(data1).unwrap();
+    let signature = signer.sign_to_vec().unwrap();
+    println!("Signature bytes: {}", std::mem::size_of_val(&signature));
+
+    // To verify the signature, we need the original data.
+    let mut verifier = Verifier::new(MessageDigest::sha256(), &keypair).unwrap();
+    verifier.update(data0).unwrap();
+    assert!(verifier.verify(&signature).unwrap());
+}
