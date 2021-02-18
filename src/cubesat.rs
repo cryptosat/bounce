8615cad5297f36d68e70a0b52c94ee0f4e2ba039
Taegyun Kim <k.taegyun@gmail.com>
2021-02-18 17:46:34 -0500

    Simple hello world service.
    Test using following commands
    1. `cargo run --bin cubesat`
    2. Then in another terminal,
    `grpcurl --plaintext -import-path ./proto -proto bounce.proto -d '{"message": "world"}' "[::1]:50051" bounce.Signer/Sign`

--- src/cubesat.rs
+++ src/cubesat.rs
@@ -1,49 +1,86 @@
 extern crate bitcoin_hashes;
 extern crate secp256k1;
+extern crate tokio;
+extern crate tonic;
 
-use bitcoin_hashes::{sha256, Hash};
-use secp256k1::rand::rngs::OsRng;
-use secp256k1::{
-    Error, Message, PublicKey, Secp256k1, SecretKey, Signature, Signing, Verification,
-};
+// use bitcoin_hashes::{sha256, Hash};
+// use secp256k1::rand::rngs::OsRng;
+// use secp256k1::{
+//     Error, Message, PublicKey, Secp256k1, SecretKey, Signature, Signing, Verification,
+// };
+use tonic::{transport::Server, Request, Response, Status};
 
-fn verify<C: Verification>(
-    secp: &Secp256k1<C>,
-    msg: &[u8],
-    sig: Signature,
-    pubkey: PublicKey,
-) -> Result<bool, Error> {
-    let msg = sha256::Hash::hash(msg);
-    let msg = Message::from_slice(&msg)?;
-    Ok(secp.verify(&msg, &sig, &pubkey).is_ok())
+use bounce::signer_server::{Signer, SignerServer};
+use bounce::{SignRequest, SignResponse};
+pub mod bounce {
+    tonic::include_proto!("bounce"); // The string specified here must match the proto package name
 }
 
-fn sign<C: Signing>(
-    secp: &Secp256k1<C>,
-    msg: &[u8],
-    seckey: &SecretKey,
-) -> Result<Signature, Error> {
-    let msg = sha256::Hash::hash(msg);
-    let msg = Message::from_slice(&msg)?;
-    Ok(secp.sign(&msg, &seckey))
+#[derive(Debug, Default)]
+pub struct Cubesat {}
+
+#[tonic::async_trait]
+impl Signer for Cubesat {
+    async fn sign(&self, request: Request<SignRequest>) -> Result<Response<SignResponse>, Status> {
+        println!("Got a request: {:?}", request);
+        let response = bounce::SignResponse {
+            signature:"hello".to_string(),
+        };
+
+        Ok(Response::new(response))
+    }
 }
 
-fn main() {
-    let secp = Secp256k1::new();
-    let mut rng = OsRng::new().unwrap();
-    // generate public and private key pairs.
-    let (seckey, pubkey) = secp.generate_keypair(&mut rng);
-    assert_eq!(pubkey, PublicKey::from_secret_key(&secp, &seckey));
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "[::1]:50051".parse()?;
+    let cubesat = Cubesat::default();
 
-    // Read message to sign
-    println!("Enter your name: ");
-    let mut name = String::new();
-    std::io::stdin()
-        .read_line(&mut name)
-        .expect("Failed to read line");
+    Server::builder()
+        .add_service(SignerServer::new(cubesat))
+        .serve(addr)
+        .await?;
 
-    println!("Hello, {}!", &name[..name.len() - 1]);
-
-    let sig = sign(&secp, name.as_bytes(), &seckey).unwrap();
-    assert!(verify(&secp, name.as_bytes(), sig, pubkey).unwrap());
+    Ok(())
 }
+
+// fn verify<C: Verification>(
+//     secp: &Secp256k1<C>,
+//     msg: &[u8],
+//     sig: Signature,
+//     pubkey: PublicKey,
+// ) -> Result<bool, Error> {
+//     let msg = sha256::Hash::hash(msg);
+//     let msg = Message::from_slice(&msg)?;
+//     Ok(secp.verify(&msg, &sig, &pubkey).is_ok())
+// }
+
+// fn sign<C: Signing>(
+//     secp: &Secp256k1<C>,
+//     msg: &[u8],
+//     seckey: &SecretKey,
+// ) -> Result<Signature, Error> {
+//     let msg = sha256::Hash::hash(msg);
+//     let msg = Message::from_slice(&msg)?;
+//     Ok(secp.sign(&msg, &seckey))
+// }
+
+// fn main() {
+//     let secp = Secp256k1::new();
+//     let mut rng = OsRng::new().unwrap();
+//     // generate public and private key pairs.
+//     let (seckey, pubkey) = secp.generate_keypair(&mut rng);
+//     assert_eq!(pubkey, PublicKey::from_secret_key(&secp, &seckey));
+
+//     // Read message to sign
+//     println!("Enter your name: ");
+//     let mut name = String::new();
+//     std::io::stdin()
+//         .read_line(&mut name)
+//         .expect("Failed to read line");
+
+//     println!("Hello, {}!", &name[..name.len() - 1]);
+
+//     let sig = sign(&secp, name.as_bytes(), &seckey).unwrap();
+//     assert!(verify(&secp, name.as_bytes(), sig, pubkey).unwrap());
+// }
