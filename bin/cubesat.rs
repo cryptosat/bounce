c98fd1c4f03e10a3a0d58b7f1d1d76c5374ae511
Taegyun Kim <k.taegyun@gmail.com>
2021-02-18 14:25:07 -0500

    Get input and sign/verify

--- bin/cubesat.rs
+++ bin/cubesat.rs
@@ -1,17 +1,49 @@
+extern crate bitcoin_hashes;
 extern crate secp256k1;
 
+use bitcoin_hashes::{sha256, Hash};
 use secp256k1::rand::rngs::OsRng;
-use secp256k1::{PublicKey, Secp256k1, SecretKey};
+use secp256k1::{
+    Error, Message, PublicKey, Secp256k1, SecretKey, Signature, Signing, Verification,
+};
+
+fn verify<C: Verification>(
+    secp: &Secp256k1<C>,
+    msg: &[u8],
+    sig: Signature,
+    pubkey: PublicKey,
+) -> Result<bool, Error> {
+    let msg = sha256::Hash::hash(msg);
+    let msg = Message::from_slice(&msg)?;
+    Ok(secp.verify(&msg, &sig, &pubkey).is_ok())
+}
+
+fn sign<C: Signing>(
+    secp: &Secp256k1<C>,
+    msg: &[u8],
+    seckey: &SecretKey,
+) -> Result<Signature, Error> {
+    let msg = sha256::Hash::hash(msg);
+    let msg = Message::from_slice(&msg)?;
+    Ok(secp.sign(&msg, &seckey))
+}
 
 fn main() {
     let secp = Secp256k1::new();
     let mut rng = OsRng::new().unwrap();
-    // First option:
+    // generate public and private key pairs.
     let (seckey, pubkey) = secp.generate_keypair(&mut rng);
-
     assert_eq!(pubkey, PublicKey::from_secret_key(&secp, &seckey));
 
-    // Second option:
-    let seckey = SecretKey::new(&mut rng);
-    let _pubkey = PublicKey::from_secret_key(&secp, &seckey);
+    // Read message to sign
+    println!("Enter your name: ");
+    let mut name = String::new();
+    std::io::stdin()
+        .read_line(&mut name)
+        .expect("Failed to read line");
+
+    println!("Hello, {}!", &name[..name.len() - 1]);
+
+    let sig = sign(&secp, name.as_bytes(), &seckey).unwrap();
+    assert!(verify(&secp, name.as_bytes(), sig, pubkey).unwrap());
 }
