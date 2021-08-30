633a0e3f5012c4fc5850ad4fa6a79fa241047911
Taegyun Kim <k.taegyun@gmail.com>
2021-08-29 21:22:49 -0400

    comments

--- src/bin/experiment.rs
+++ src/bin/experiment.rs
@@ -26,3 +26,39 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     Ok(())
 }
+
+// Reads a file which contains Bounce configuration
+// Bounce configuration consist of the following
+// 1. Ground Station configuration
+//  - IP address, port
+//  - number of ground stations
+// 2. Flock configuration
+//  - IP address, port
+//  - number of bounce units in the flock
+//  - failure modes for each of the bounce units
+// 3. Slot configuration
+//  - the length of the slot and each phases
+// 4. Experiment configuration
+//  - logging options (log to stdout or log file directory)
+
+// Start
+// Timer server
+// space station
+// flock
+// Wait for above to be ready
+// Then start the experiment
+
+// how to represent the configuration
+// 1. using a protobuf
+// 2. using a json file
+// 3. Using a plain text file
+
+// Representation of the experiment configuration
+// - Experiment strategy
+//  1. random
+//  2. predefined with a pattern
+//  3. All manual
+
+//
+
+// How to make sure each experiment is consistent over runs.
